ee14ad8d82468f5edc8d3f1528e89a9e672e7f58
raulmatrix <raul.vera.portanda@gmail.com>
2022-10-10 01:05:56 -0400

    formulario terminado

--- app/src/main/java/com/example/proyectopanaderia/Formulario.java
+++ app/src/main/java/com/example/proyectopanaderia/Formulario.java
@@ -11,4 +11,9 @@ public class Formulario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
     }
+
+
+    public void registrarEstudiante(){
+
+    }
 }
--- app/src/main/java/com/example/proyectopanaderia/MainActivity.java
+++ app/src/main/java/com/example/proyectopanaderia/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.proyectopanaderia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    int tiempoTranscurrir = 5000; //1 segundo, 1000 millisegundos.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+
+
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //***Aquí agregamos el proceso a ejecutar.
+
+                Intent intent = new Intent(getApplicationContext(), Formulario.class);
+                startActivity(intent);
+
+                handler.removeCallbacks(null);
+            }
+        }, tiempoTranscurrir );//define el tiempo.
     }
 }
